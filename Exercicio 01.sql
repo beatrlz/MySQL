eba8c007f48e6c01e1e36d73b731e4b8ccd66ad4
Beatriz Lima <87488777+beatrlz@users.noreply.github.com>
2021-08-19 23:56:02 -0300

    Exercicios 19.08

--- Exercicio 01.sql
+++ Exercicio 01.sql
@@ -1,27 +1,43 @@
-create database rh_funcionarios;
+create database db_generation_game_online;
 
-use rh_funcionarios;
+use db_generation_game_online;
 
-create table tb_funcionarios(
+create table tb_classe(
 id bigint auto_increment,
-nome varchar(255) not null,
-funcao varchar(80),
-setor varchar(50),
-salario decimal(8,2) not null,
+classe varchar(200)not null,
 primary key (id)
 );
 
-insert into tb_funcionarios (nome, funcao, setor, salario) values ("Gabriela", "desenvolvedora java", "TI", 2500.00);
-insert into tb_funcionarios (nome, funcao, setor, salario) values ("Gabriel", "analista de suporte técnico", "TI", 3000.00);
-insert into tb_funcionarios (nome, funcao, setor, salario) values ("Ana Clara", "analista de aplicativo", "TI", 4530.00);
-insert into tb_funcionarios (nome, funcao, setor, salario) values ("Bernardo", "engenheiro de software", "TI", 5430.00);
-insert into tb_funcionarios (nome, funcao, setor, salario) values ("Pedro", "programador de internet", "TI", 2300.00);
+insert into tb_classe (classe) values ("Feiticeira");
+insert into tb_classe (classe) values ("Mago");
+insert into tb_classe (classe) values ("Guerreiro");
+insert into tb_classe (classe) values ("Vingador");
+insert into tb_classe (classe) values ("Bruxo");
 
-select * from tb_funcionarios;
+select * from tb_classe;
 
-select * from tb_funcionarios where salario >2000;
-select * from tb_funcionarios where salario <2000;
- 
- update tb_funcionarios set nome = "Bernardo" where id = 1;
- 
- 
+create table tb_personagem(
+id bigint auto_increment,
+nome varchar(255)not null,
+ataque int,
+defesa int,
+classe_id bigint,
+primary key (id),
+foreign key(classe_id) references tb_classe (id)
+);
+
+insert into tb_personagem (nome, ataque, defesa, classe_id) values ("Escarlate", 1000, 2000, 2);
+insert into tb_personagem (nome, ataque, defesa, classe_id) values ("Belchior", 1000, 5000, 5);
+insert into tb_personagem (nome, ataque, defesa, classe_id) values ("Alaunus", 2500, 4000, 3);
+insert into tb_personagem (nome, ataque, defesa, classe_id) values ("Capitão América", 1600, 4500, 4);
+insert into tb_personagem (nome, ataque, defesa, classe_id) values ("Morgan", 2000, 1500, 1);
+
+select * from tb_personagem;
+
+select * from tb_personagem where ataque > 2000;
+select * from tb_personagem where ataque > 1000 and ataque < 2000;
+
+select * from tb_personagem where nome like "%C%";
+
+select * from tb_personagem inner join tb_classe on tb_personagem.classe_id = tb_classe.id;
+select * from tb_personagem inner join tb_classe on tb_personagem.classe_id where classe_id = 4;
